2acbfe1b1d2f9333bd900866f84e36a2a053fb67
ONJoseph <on.josephinfo@gmail.com>
2023-05-11 15:43:32 +0100

    Add controllers integration tests

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+require 'rails_helper'
+
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get welcome_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get welcome_create_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+end
