6bdf1c632dfa24fb57690df9ec79b564675a3caf
ONJoseph <on.josephinfo@gmail.com>
2023-05-11 15:47:10 +0100

    Add integration tests

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,19 +1,18 @@
-require "test_helper"
+require 'test_helper'
 require 'rails_helper'
 
-
 class GroupsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get groups_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get groups_create_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get groups_show_url
     assert_response :success
   end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,19 +1,18 @@
-require "test_helper"
+require 'test_helper'
 require 'rails_helper'
 
-
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get welcome_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get welcome_create_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get welcome_index_url
     assert_response :success
   end
